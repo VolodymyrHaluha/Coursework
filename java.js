5ddbd15f39beded3ccae958a46462ff4b554e67f
VolodymyrHaluha <galuga2005@icloud.com>
2023-11-28 18:56:02 +0200

    Site is done< now i should add data base b-e

--- java.js
+++ java.js
@@ -13,3 +13,22 @@ icon.addEventListener('click', function() {
         popupContainer.classList.add('hidden');
     }
 });
+document.addEventListener('DOMContentLoaded', function () {
+    const orderButtons = document.querySelectorAll('.order-button');
+
+    orderButtons.forEach(button => {
+        button.addEventListener('click', function () {
+            const productId = this.closest('.sale_section_block1, .sets_section_block2, .sale_section_block3, .sets_section_block4').dataset.id;
+            addToCart(productId);
+        });
+    });
+
+    function addToCart(productId) {
+        // Отримайте дані товару за його ідентифікатором (productId)
+        // Додайте товар у кошик, наприклад, зберігаючи його в локальному сховищі
+        const cart = JSON.parse(localStorage.getItem('cart')) || [];
+        cart.push(productId);
+        localStorage.setItem('cart', JSON.stringify(cart));
+        alert('Товар додано в кошик!');
+    }
+});
